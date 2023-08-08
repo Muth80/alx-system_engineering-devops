3c256affaa59f346e7c92a8f3da6c1a9270a9472
David Muindi <muindidiego@gmail.com>
2023-08-08 06:15:08 -0700

    Tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    return response.json().get("data").get("subscribers")
